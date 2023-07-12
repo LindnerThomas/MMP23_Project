8be7d344f3bae4471e8de41d4377aa13e7e4c80c
walterlars <lars.michelbach@web.de>
2023-07-12 23:54:39 +0200

    background music + button sounds

--- BloomPaw/Assets/Scripts/MainMenu.cs
+++ BloomPaw/Assets/Scripts/MainMenu.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+
+   public AudioClip audio;
+   public AudioSource audioSource;
+
+   //public void playButtonAudio(){
+   //   audio.Play();
+   //}
    public void PlayGame(){
+       audioSource.Play();
+      StartCoroutine(_PlayGame());
+   }
+
+   public void GoBack(){
+      audioSource.Play();
+      StartCoroutine(_GoBack());
+   }
+ 
+   private IEnumerator _PlayGame(){
+        yield return new WaitForSeconds(audio.length);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
    }
-
-   public void ToFlowershop(){
-      SceneManager.LoadScene(2);
+   
+   private IEnumerator _GoBack(){
+        yield return new WaitForSeconds(audio.length);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
    }
 
-    public void ToGarden(){
-      SceneManager.LoadScene(4);
-   }
    public void QuitGame(){
         Application.Quit();
         Debug.Log("Quit works");
