78afbccf72e7923c2388ae8b80a3862aa2294652
scpxiao <64504003+scpxiao@users.noreply.github.com>
2023-07-18 03:45:17 +0200

    Revert "added 3 customers with random orders in speech bubbles to flower shop"
    This reverts commit ab523f94b91ddce17192280e6fbcf7f02e97c244.

--- BloomPaw/Assets/Scripts/MainMenu.cs
+++ BloomPaw/Assets/Scripts/MainMenu.cs
@@ -5,40 +5,35 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    public AudioClip audio;
-    public AudioSource audioSource;
+   public AudioClip audio;
+   public AudioSource audioSource;
 
-    //public void playButtonAudio(){
-    //   audio.Play();
-    //}
-    public void PlayGame()
-    {
-        audioSource.Play();
-        StartCoroutine(_PlayGame());
-    }
+   //public void playButtonAudio(){
+   //   audio.Play();
+   //}
+   public void PlayGame(){
+       audioSource.Play();
+      StartCoroutine(_PlayGame());
+   }
 
-    public void GoBack()
-    {
-        audioSource.Play();
-        StartCoroutine(_GoBack());
-    }
-
-    private IEnumerator _PlayGame()
-    {
+   public void GoBack(){
+      audioSource.Play();
+      StartCoroutine(_GoBack());
+   }
+ 
+   private IEnumerator _PlayGame(){
         yield return new WaitForSeconds(audio.length);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    private IEnumerator _GoBack()
-    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+   }
+   
+   private IEnumerator _GoBack(){
         yield return new WaitForSeconds(audio.length);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+   }
 
-    public void QuitGame()
-    {
+   public void QuitGame(){
         audioSource.Play();
         Application.Quit();
         Debug.Log("Quit works");
-    }
+   }
 }
